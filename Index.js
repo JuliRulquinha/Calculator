5bc20fb8048e78ec0d22a6301443c05c5088e62a
JuliRulquinha <juli-felix@outlook.com>
2024-08-23 13:09:19 -0300

    added all the numbers functions

--- Index.js
+++ Index.js
@@ -13,7 +13,7 @@ function Calculate()
 function clearAll()
 {
     let value = document.getElementById("btn-clear-all");
-    let input = document.getElementById("txt-display")
+    let input = document.getElementById("txt-display");
     input.value = "";
 }
 
@@ -26,5 +26,66 @@ function btnOneClick()
 
 function btnTwoClick()
 {
-    let value = document.getElementById()
+    let value = document.getElementById("btn-two").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
 }
+
+function btnThreeClick()
+{
+    let value = document.getElementById("btn-three").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
+}
+
+function btnFourClick()
+{
+    let value = document.getElementById("btn-four").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
+
+}
+
+function btnFiveClick()
+{
+    let value = document.getElementById("btn-five").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
+}
+
+function btnSixClick()
+{
+    let value = document.getElementById("btn-six").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
+}
+
+function btnSevenClick()
+{
+    let value = document.getElementById("btn-seven").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
+}
+
+function btnEightClick()
+{
+    let value = document.getElementById("btn-eight").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
+}
+
+function btnNineClick()
+{
+    let value = document.getElementById("btn-nine").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
+}
+
+function btnZeroClick()
+{
+    let value = document.getElementById("btn-zero").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += value;
+}
+
+
