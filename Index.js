04e304076212b593bdb33153ef19fa8f906638ed
JuliRulquinha <juli-felix@outlook.com>
2024-08-23 12:52:50 -0300

    added clear all function

--- Index.js
+++ Index.js
@@ -1,27 +1,30 @@
+const display = document.getElementById("display");
 
-function typeToDisplay(input)
+function typeToDisplay()
 {
-
+    display.value += input;
 }
 
-function showResult()
+function Calculate()
 {
 
 }
 
 function clearAll()
 {
-
-}
-
-function clearEntry()
-{
-
+    let value = document.getElementById("btn-clear-all");
+    let input = document.getElementById("txt-display")
+    input.value = "";
 }
 
 function btnOneClick()
 {
     let value = document.getElementById("btn-one").innerText;
     let input = document.getElementById("txt-display");
-    input.value = value;
+    input.value += value;
+}
+
+function btnTwoClick()
+{
+    let value = document.getElementById()
 }
