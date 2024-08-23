ea68fc1b3f24536fe5e169be20e7b376b7a91f7f
JuliRulquinha <juli-felix@outlook.com>
2024-08-23 17:39:28 -0300

    added multiple functions

--- Index.js
+++ Index.js
@@ -1,12 +1,62 @@
 const display = document.getElementById("display");
+const getById = document.getElementById;
+getById.nome = "sei la";
 
-function typeToDisplay()
+function calculate()
 {
-    display.value += input;
-}
+   //Get the expression ex: 1+1
+    
+    let input = document.getElementById("txt-display");    
+    let expression = input.value;
 
-function Calculate()
-{
+
+
+    let operator = "";
+    let operatorIndex = 0;
+
+    
+   //Check if there is an operator
+   let plusSignIndex = expression.indexOf('+');
+   let minusSignIndex = expression.indexOf('-');
+   let divisionSignIndex = expression.indexOf('÷');
+   let timesSignIndex = expression.indexOf('*');
+
+   if(plusSignIndex > 0) {
+    operator = "+";
+    operatorIndex = plusSignIndex;
+   }
+
+   else if(minusSignIndex > 0) {
+    operator = "-";
+    operatorIndex = minusSignIndex;
+   }
+
+   else if(divisionSignIndex > 0) {
+    operator = "÷";
+    operatorIndex = divisionSignIndex;
+   }
+
+   else if(timesSignIndex > 0) {
+    operator = "*";
+    operatorIndex = timesSignIndex;
+   }
+
+   //Check if there is a number before and after the operator
+
+   let operatorIsAtAValidPosition = operatorIndex < expression.length-1 || operatorIndex > 0;
+
+   if(operatorIsAtAValidPosition)
+   {
+       debugger; 
+        let firstNumber = expression.substring(0,operatorIndex);
+        let secondNumber = expression.substring(operatorIndex+1,expression.length);
+        let operation = selectFunctionByOperator(operator);
+        input.value = operation(+firstNumber,+secondNumber);
+
+   }
+   //Check which expression needs to be applied according to the operator
+   //Run the expression and show the results -> Check for possible errors
+
 
 }
 
@@ -89,3 +139,80 @@ function btnZeroClick()
 }
 
 
+function btnSumClick()
+{
+    
+    let operator = document.getElementById("plus-sign").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += operator;
+
+}
+
+function btnSubtractClick()
+{
+    
+    let operator = document.getElementById("btn-minus").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += operator;
+
+}
+
+function btnDivideClick()
+{
+    
+    let operator = document.getElementById("btn-divide").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += operator;
+
+}
+
+function btnMultiplyClick()
+{
+    
+    let operator = document.getElementById("btn-multiply").innerText;
+    let input = document.getElementById("txt-display");
+    input.value += operator;
+
+}
+
+
+ 
+function selectFunctionByOperator(operator)
+{
+    switch(operator)
+    {
+        case '+': return Sum;
+        break;
+
+        case '-': return Subtract;
+        break;
+
+        case '÷': return Divide;
+        break;
+
+        case '*': return Multiply;
+        break;
+
+        default: console.log("Error");
+    }
+}
+
+function Sum(a, b)
+{
+    return a+b;
+}
+
+function Subtract(a, b)
+{
+    return a-b;
+}
+
+function Multiply(a, b)
+{
+    return a*b;
+}
+
+function Divide(a, b)
+{
+    return a/b;
+}
